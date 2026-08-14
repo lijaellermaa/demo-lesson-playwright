34f92c23ad752b47fe84435969fd3b7d8a0efe88
lellermaa <lellermaa@gmail.com>
2026-08-14 12:54:45 +0300

    feat: add component tests with mocks

---
+++ utils/test-data.ts
@@ -0,0 +1,10 @@
+export const TEST_DATA = {
+  CREATE_ORDER_RESPONSE: {
+    status: 'OPEN',
+    courierId: null,
+    customerName: 'Qwerty',
+    customerPhone: '1234567',
+    comment: 'qwertyqwertyqwerty',
+    id: 9999,
+  },
+}
